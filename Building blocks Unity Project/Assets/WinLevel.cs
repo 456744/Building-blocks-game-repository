1c7b9dcd4403237ff2a82119a1be5252f51bbec9
456744 <ben@kinnaird.org.uk>
2022-01-09 23:28:41 +0000

    implemented menu and level transitions

--- Building blocks Unity Project/Assets/WinLevel.cs
+++ Building blocks Unity Project/Assets/WinLevel.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class WinLevel : MonoBehaviour
 {
+
+    public string Level;
     void OnCollisionEnter(Collision collision)
     {
 
         //Checks if the desired object was created
         if (collision.gameObject.tag == "Objective")
         {
-            Debug.LogWarning("Win!");
+            SceneManager.LoadScene(Level);
 
         }
 
