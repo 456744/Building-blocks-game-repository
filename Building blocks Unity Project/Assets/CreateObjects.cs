5564a231905f56568d6c1c805e608c4a3c3117af
456744 <ben@kinnaird.org.uk>
2022-01-09 17:14:48 +0000

    got object creation for first level done and added "pick up" function

--- Building blocks Unity Project/Assets/CreateObjects.cs
+++ Building blocks Unity Project/Assets/CreateObjects.cs
@@ -5,24 +5,151 @@ using UnityEngine;
 public class CreateObjects : MonoBehaviour
 {
 
+    public int createHeat = 0;
+
+    public GameObject HeatPrefab;
+
     public int createSteam = 0;
 
     public GameObject SteamPrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int createMud = 0;
+
+    public GameObject MudPrefab;
+
+    public int createCloud = 0;
+
+    public GameObject CloudPrefab;
+
+    public int createLightning = 0;
+
+    public GameObject LightningPrefab;
+
+    public int createGlass = 0;
+
+    public GameObject GlassPrefab;
+
+    public int createSand = 0;
+
+    public GameObject SandPrefab;
+
+    public int createClay = 0;
+
+    public GameObject ClayPrefab;
+
+    public int createBrick = 0;
+
+    public GameObject BrickPrefab;
+
+    public int createBuilding = 0;
+
+    public GameObject BuildingPrefab;
+
+    public int createSmoke = 0;
+
+    public GameObject SmokePrefab;
+
+    public int createFood = 0;
+
+    public GameObject FoodPrefab;
+
+    public int createOffering = 0;
+
+    public GameObject OfferingPrefab;
+
 
     // Update is called once per frame
     void Update()
     {
+        if (createHeat >= 2)
+        {
+            createHeat -= 2;
+
+            Instantiate(HeatPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
         if (createSteam >= 2)
         {
             createSteam -= 2;
 
             Instantiate(SteamPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
+        if (createMud >= 2)
+        {
+            createMud -= 2;
+
+            Instantiate(MudPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
+        if (createCloud >= 2)
+        {
+            createCloud -= 2;
+
+            Instantiate(CloudPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
+        if (createLightning >= 2)
+        {
+            createLightning -= 2;
+
+            Instantiate(LightningPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
+        if (createGlass >= 2)
+        {
+            createGlass -= 2;
+
+            Instantiate(GlassPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
+        if (createSand >= 2)
+        {
+            createSand -= 2;
+
+            Instantiate(SandPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
+        if (createClay >= 2)
+        {
+            createClay -= 2;
+
+            Instantiate(ClayPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
+        if (createBrick >= 2)
+        {
+            createBrick -= 2;
+
+            Instantiate(BrickPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
+        if (createBuilding >= 2)
+        {
+            createBuilding -= 2;
+
+            Instantiate(BuildingPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
+        if (createSmoke >= 2)
+        {
+            createSmoke -= 2;
+
+            Instantiate(SmokePrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
+        if (createFood >= 2)
+        {
+            createFood -= 2;
+
+            Instantiate(FoodPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
+        }
+
+        if (createOffering >= 2)
+        {
+            createOffering -= 2;
+
+            Instantiate(OfferingPrefab, new Vector3(Random.Range(0, 5), 5, Random.Range(0, 5)), Quaternion.identity);
         }
 
 
--- Building blocks Unity Project/Assets/ObjectCollision.cs
+++ Building blocks Unity Project/Assets/ObjectCollision.cs
@@ -7,20 +7,121 @@ public class ObjectCollision : MonoBehaviour
 
     CreateObjects progress = null;
 
+    public GameObject Hold;
+
     private void Start()
     {
         progress = GameObject.Find("ObjectCreator").GetComponent<CreateObjects>();
+
+        Hold = GameObject.Find("Hold object");
     }
 
     //Detect collisions between the GameObjects with Colliders attached
     void OnCollisionEnter(Collision collision)
     {
 
-        //Check for a match with the specific tag on any GameObject that collides with your GameObject
-        if (collision.gameObject.tag == "Fire" || collision.gameObject.tag == "Water")
+        //Checks if the game object and colliding object should make a heat object
+        if (collision.gameObject.tag == "Fire" && gameObject.tag == "Air" || collision.gameObject.tag == "Air" && gameObject.tag == "Fire")
         {
-            //If the GameObject has the same tag as specified, create a prefab object
+            //tells the object creating script to create the desired prefab
+            progress.createHeat++;
+        }
+
+        //Checks if the game object and colliding object should make a steam object
+        if (collision.gameObject.tag == "Water" && gameObject.tag == "Heat" || collision.gameObject.tag == "Heat" && gameObject.tag == "Water")
+        {
+            //tells the object creating script to create the desired prefab
             progress.createSteam++;
         }
+
+        //Checks if the game object and colliding object should make a mud object
+        if (collision.gameObject.tag == "Earth" && gameObject.tag == "Water" || collision.gameObject.tag == "Water" && gameObject.tag == "Earth" || collision.gameObject.tag == "Earth" && gameObject.tag == "Cloud" || collision.gameObject.tag == "Cloud" && gameObject.tag == "Earth")
+        {
+            //tells the object creating script to create the desired prefab
+            progress.createMud++;
+        }
+
+        //Checks if the game object and colliding object should make a cloud object
+        if (collision.gameObject.tag == "Water" && gameObject.tag == "Air" || collision.gameObject.tag == "Air" && gameObject.tag == "Water")
+        {
+            //tells the object creating script to create the desired prefab
+            progress.createCloud++;
+        }
+
+        //Checks if the game object and colliding object should make a lightning object
+        if (collision.gameObject.tag == "Fire" && gameObject.tag == "Cloud" || collision.gameObject.tag == "Cloud" && gameObject.tag == "Fire")
+        {
+            //tells the object creating script to create the desired prefab
+            progress.createLightning++;
+        }
+
+        //Checks if the game object and colliding object should make a glass object
+        if (collision.gameObject.tag == "lightning" && gameObject.tag == "Earth" || collision.gameObject.tag == "Earth" && gameObject.tag == "lightning" || collision.gameObject.tag == "Fire" && gameObject.tag == "Sand" || collision.gameObject.tag == "Sand" && gameObject.tag == "Fire")
+        {
+            //tells the object creating script to create the desired prefab
+            progress.createGlass++;
+        }
+
+        //Checks if the game object and colliding object should make a sand object
+        if (collision.gameObject.tag == "Glass" && gameObject.tag == "Earth" || collision.gameObject.tag == "Earth" && gameObject.tag == "Glass")
+        {
+            //tells the object creating script to create the desired prefab
+            progress.createSand++;
+        }
+
+        //Checks if the game object and colliding object should make a clay object
+        if (collision.gameObject.tag == "Sand" && gameObject.tag == "Water" || collision.gameObject.tag == "Water" && gameObject.tag == "Sand")
+        {
+            //tells the object creating script to create the desired prefab
+            progress.createClay++;
+        }
+
+        //Checks if the game object and colliding object should make a Brick object
+        if (collision.gameObject.tag == "Fire" && gameObject.tag == "Clay" || collision.gameObject.tag == "Clay" && gameObject.tag == "Fire")
+        {
+            //tells the object creating script to create the desired prefab
+            progress.createBrick++;
+        }
+
+        //Checks if the game object and colliding object should make a building object
+        if (collision.gameObject.tag == "Brick" && gameObject.tag == "Mud" || collision.gameObject.tag == "Mud" && gameObject.tag == "Brick")
+        {
+            //tells the object creating script to create the desired prefab
+            progress.createBuilding++;
+        }
+
+        //Checks if the game object and colliding object should make a smoke object
+        if (collision.gameObject.tag == "Fire" && gameObject.tag == "Steam" || collision.gameObject.tag == "Steam" && gameObject.tag == "Fire")
+        {
+            //tells the object creating script to create the desired prefab
+            progress.createSmoke++;
+        }
+
+        //Checks if the game object and colliding object should make a food object
+        if (collision.gameObject.tag == "Smoke" && gameObject.tag == "Building" || collision.gameObject.tag == "Building" && gameObject.tag == "Smoke")
+        {
+            //tells the object creating script to create the desired prefab
+            progress.createFood++;
+        }
+
+        //Checks if the game object and colliding object should make an offering object
+        if (collision.gameObject.tag == "Food" && gameObject.tag == "lightning" || collision.gameObject.tag == "lightning" && gameObject.tag == "Food")
+        {
+            //tells the object creating script to create the desired prefab
+            progress.createOffering++;
+        }
     }
+
+    private void OnMouseDown()
+    {
+        if (transform.parent == null)
+        {
+            gameObject.transform.parent = Hold.transform;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+    }
+
 }
